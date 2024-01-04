c26606c3388d7b3a0462539ea580891ff1685173
Jens Koenen <koenen@vis.rwth-aachen.de>
2024-01-04 16:48:58 +0100

    Worked on wizard

---
+++ website/components/wizard_file_metadata.js
@@ -0,0 +1,12 @@
+const file_dictonary = 
+[
+    { extension: ".vti", dimension: "", }
+];
+
+function check_file(file)
+{
+
+}
+
+
+
---
+++ website/components/wizard_question_check.js
@@ -0,0 +1,39 @@
+import { computed } from "vue"
+
+export default
+{
+    props: ["answers", "options"],
+    setup(props)
+    {
+        let wizard_question_groups = computed(() =>
+        {
+            let groups = [];
+
+            for(const option of props.options)
+            {
+                if(!groups.some(item => item == option.group))
+                {
+                    groups.push(option.group);
+                }
+            }
+
+            return groups;
+        });
+
+        return {
+            wizard_question_groups
+        }
+    },
+    template:
+    `
+    <div class="d-flex flex-wrap p-3">
+        <div v-for="group of wizard_question_groups" class="me-4 mb-4">
+            <h5 >{{ group }}</h5>
+            <div v-for="option of options.filter(item => item.group == group)" class="form-check ms-2">
+                <input :id="'wizard_question_option_' + option.name" class="form-check-input" type="checkbox">
+                <label :for="'wizard_question_option_' + option.name" class="form-check-label">{{ option.title }}</label>
+            </div>
+        </div>
+    </div>
+    `
+}
