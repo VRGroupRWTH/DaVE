8229dfe0f37952c1eef5f1b8459d2bd7a480c45f
Marvin Petersen <m_petersen12@cs.uni-kl.de>
2024-01-19 14:26:12 +0000

    Add multiple slices example

---
+++ database/multiple_slices/description.md
@@ -0,0 +1,18 @@
+#### Description ####
+Slices show a cut throught the data.In this case three slices are shown, each aligned with an axis. Furhtermore, this example produces a Cinema database file for data exploration after rendering. With, for example, [Cinema View](https://github.com/cinemascience/cinema_view) these .cdb files can be displayed.
+
+#### Instructions ####
+To execute the example just run
+
+```
+./multiple_slice_script.sh
+```
+
+If the script is not executible run the following command
+
+```
+chmod +x miltpile_slice_script.sh
+```
+
+#### Limitations ####
+The Cinema Viewer is not part of this example and has to be downloaded sperately. 
---
+++ database/multiple_slices/resources/multiple_slice_script.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -e
+
+# check for the existence of data and use default when none found
+mkdir -p dataset
+mkdir -p output
+if ! test -f "${DATASET_VOLUME}"; then
+    echo "data set '${DATASET_VOLUME}' not found - using default"
+    DATASET_VOLUME="./dataset/ctBones.vti"
+    if ! test -f "${DATASET_VOLUME}"; then
+	    cd dataset
+	    wget "https://raw.githubusercontent.com/topology-tool-kit/ttk-data/dev/ctBones.vti"
+	    cd ..
+    fi;
+fi;
+
+# determine additional execution commands for slurm and mpi
+EXEC=""
+case "${EXEC_TYPE}" in
+    mpi)
+        EXEC="mpirun --allow-run-as-root"
+        ;;
+
+    slurm)
+        EXEC="srun -n 2 --time=2"
+        ;;
+esac
+
+# assemble run command for docker
+if [[ "${CONTAINER_PLATFORM}" == "docker" ]]; then
+    docker run --rm -v .:/example -w /example "${CONTAINER_URL}" ${EXEC} ${COMMAND} "${DATASET_VOLUME}"
+fi;
+
+# assemble run command for singularity
+if [[ "${CONTAINER_PLATFORM}" == "singularity" ]]; then
+    ${EXEC} singularity run --containall  -H "${PWD}:/example" "docker://${CONTAINER_URL}" ${COMMAND} "${DATASET_VOLUME}"
+fi;
