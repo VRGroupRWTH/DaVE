3f13765942e7cc247e84df15990c523083c793f0
Jens Koenen <koenen@vis.rwth-aachen.de>
2024-02-09 13:36:38 +0100

    Worked on guide

---
+++ website/components/shared_container.js
@@ -0,0 +1,14 @@
+export const SharedContainer = 
+{
+    template:
+    `
+    <div class="container-xxl">
+        <div>
+            <slot></slot>
+        </div>
+        <div style="background-color: gray; width: 300px; min-height: 200px">
+
+        </div>
+    </div>
+    `
+};
--- website/components/shared_footer.js
+++ website/components/shared_footer.js
@@ -2,7 +2,7 @@ export const SharedFooter =
 {
     template:
     `
-    <div class="d-flex flex-column flex-md-row algin-items-center justify-content-center px-4">
+    <div class="d-flex flex-column flex-md-row algin-items-center justify-content-center px-4 py-5 pb-4 border-bottom">
         <div class="flex-fill row row-cols-2 row-cols-xl-4 g-4 order-md-1">
             <div class="d-flex justify-content-center align-items-center">
                 <img class="w-100" style="max-width: 160px" src="images/nhr_4_cse.svg">
@@ -17,7 +17,25 @@ export const SharedFooter =
                 <img class="w-100" style="max-width: 200px" src="images/rwth_aachen.png">
             </div>
         </div>
-        <table class="footer-table order-md-0 mt-5 mt-md-0 me-md-4 align-self-center">
+        <div class="row row-cols-2">
+            <div class="col">
+                <div class="footer-header">Tim Gerrits</div>
+                <div class="footer-e-mail">gerrits@vis.rwth-aachen.de</div>
+            </div>
+            <div class="col">
+                <div class="footer-header">Christoph Garth</div>
+                <div class="footer-e-mail">garth@rptu.de</div>
+            </div>
+            <div class="col">
+                <div class="footer-header">Jens Koenen</div>
+                <div class="footer-e-mail">koenen@vis.rwth-aachen.de</div>
+            </div>
+            <div class="col">
+                <div class="footer-header">Marvin Petersen</div>
+                <div class="footer-e-mail">m.petersen@rptu.de</div>
+            </div>
+        </div>
+        <!--<table class="footer-table order-md-0 mt-5 mt-md-0 me-md-4 align-self-center">
             <tr>
                 <td class="px-2">Tim Gerrits</td>
                 <td><div class="border-start my-1 px-2">gerrits@vis.rwth-aachen.de</div></td>
@@ -34,7 +52,7 @@ export const SharedFooter =
                 <td class="px-2">Jens Koenen</td>
                 <td><div class="border-start my-1 px-2">koenen@vis.rwth-aachen.de</div></td>
             </tr>
-        </table>
+        </table>-->
     </div>
     `
 }
