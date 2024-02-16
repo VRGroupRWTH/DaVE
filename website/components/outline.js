993a5d609863683330023f3c65fca8850350ad5e
Jens Koenen <koenen@vis.rwth-aachen.de>
2024-02-16 16:40:53 +0100

    Worked on outline component

--- website/components/outline.js
+++ website/components/outline.js
@@ -1,8 +1,78 @@
-export default
+import { ref } from "vue"
+
+export const Outline = 
 {
-    props: ["sections"],
+    props: ["target"],
+    setup(props)
+    {
+        function on_elements_intersect(entities)
+        {
+            
+        }
+
+        const outline_intersection_options = 
+        {
+            root: null,
+            threshold: [0.1, 0.5, 1.0],
+            rootMargin: "-80px 0px 0px 0px"
+        };
+
+        let outline_intersection_observer = new IntersectionObserver(entries => on_intersection(entries), intersection_options);
+
+        let outline_container = ref(null);
+        let outline_elements = ref([]);
+        let outline_items = computed( () =>
+        {
+            let items = [];
+
+            for(const element of outline_elements)
+            {
+                const item = 
+                {
+                    label: element.innerHtml,
+                    link: "#" + element.id
+                }
+
+                items.push(item);
+            }
+
+            return items;
+        });
+
+
+        function setup_elements()
+        {
+            outline_elements.value = [];
+            search_elements(props.target)
+
+
+
+        }
+
+        function search_elements(element)
+        {
+            if(element.id != "")
+            {
+                outline_elements.value.push(element);
+            }
+
+            for(const child of element.childNodes)
+            {
+                search_elements(child);
+            }
+        }
+
+        return {
+            outline_container,
+            outline_items
+        }
+    },
     template: 
     `
-        
+    <nav ref="outline_container" class="nav flex-column align-items-stretch outline">
+        <template v-for="item of outline_items">
+            <a :class="" :href="item.link">{{ item.label }}</a>
+        </template>
+    </nav>
     `
 }
