b19776eed733dcc4d758561fa89675dc9a482a23
Marvin Petersen <m_petersen12@cs.uni-kl.de>
2024-04-03 18:01:19 +0200

    Fix check whether execution of example successfully returned

--- tests/execute_all_examples.sh
+++ tests/execute_all_examples.sh
@@ -14,10 +14,14 @@ c=0
 for entry in *.sh
 do
     c=$((c+1))
-    echo "${s}/${count} execute ${entry} ..."
+    echo "${c}/${count} execute ${entry} ..."
     chmod +x ${entry}
-    if ./${entry} | indent ; test ${PIPESTATUS[0]} -eq 0 ; then
+    ./${entry} | indent
+    RC=${PIPESTATUS[0]}
+    if [ "${RC}" = "0" ] ; then
 	s=$((s+1))
+    else
+	echo "${entry} failed"
     fi
 done
-echo "Successfully completed ${c}/${count} examples"
+echo "Successfully completed ${s}/${count} examples"
