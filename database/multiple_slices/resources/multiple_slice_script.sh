8a844162c80a13c5f1652bbe353b49e76d2ebd3a
Marvin Petersen <m_petersen12@cs.uni-kl.de>
2024-04-08 15:44:25 +0200

    Split all singularity run calls into pull and run
    - with "srun singularity run 'some_docker_image' [..]" the image was pulled multiple times
      and no cache was used
    - now the image is pulled first and then "srun singularity [..]" is called

--- database/multiple_slices/resources/multiple_slice_script.sh
+++ database/multiple_slices/resources/multiple_slice_script.sh
@@ -37,5 +37,6 @@ fi;
 
 # assemble run command for singularity
 if [[ "${CONTAINER_PLATFORM}" == "singularity" ]]; then
-    ${EXEC} singularity run --containall  -H "${PWD}:/example" "docker://${CONTAINER_URL}" ${COMMAND} "${DATASET_VOLUME_PATH}"
+    singularity pull --force container.sif "docker://${CONTAINER_URL}"
+    ${EXEC} singularity run --containall  -H "${PWD}:/example" container.sif  ${COMMAND} "${DATASET_VOLUME_PATH}"
 fi;
--- database/parallel_coords/resources/parallel_coordinates_script.sh
+++ database/parallel_coords/resources/parallel_coordinates_script.sh
@@ -35,5 +35,6 @@ fi;
 
 # assemble run command for singularity
 if [[ "${CONTAINER_PLATFORM}" == "singularity" ]]; then
-    ${EXEC} singularity run --containall  -H "${PWD}:/example" "docker://${CONTAINER_URL}" ${COMMAND} "${DATA_PATH}"
+    singularity pull --force container.sif "docker://${CONTAINER_URL}"
+    ${EXEC} singularity run --containall  -H "${PWD}:/example" container.sif  ${COMMAND} "${DATA_PATH}"
 fi;
