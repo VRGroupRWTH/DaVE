584f0b03dcd46f066af6d604fb2289a35e32c0eb
Tim Gerrits <gerrits@vis.rwth-aachen.de>
2024-04-24 11:37:08 +0200

    Added description for brain data and changed tensor glyph trace file

--- database/point_connectivity_brain/description.md
+++ database/point_connectivity_brain/description.md
@@ -1,5 +1,14 @@
 ### Description ###
-TODO
+Graph networks can represent a large variety of phenomena and their visualization can show different levels of abstraction.
+In this example, the network nodes represent neurons within a human brain, such that they have a physical location assigned to them.
+Such point-like locations in 3D space can be represented by geomtric objects, such as spherical glyphs, while connections between nodes can be represented by connecting lines.
+Properties such as size or color of glyphs and lines can then be used to encode additional attributes.
+
+This specific example is a single snapshot of one member of a brain simulation ensembles provided in the 2023 IEEE SciVis Contest [1](#reference_dataset).
+The data is based on clinical measurements of actual human brains and activity is simulated based on the model of strucural plasticity [2](#plasticity).
+Each individual neuron has target calcium levels assigned to them as well as a corresponding area in the brain.
+The target level is mapped to the glyph's color, while the different areas are visible through the coloring of the connecting lines representing synapses.
 
 ### References ###
-2. [<span id="reference_dataset">T. Gerrits, F. Czappa, D. Baneshund F. Wolf, "IEEE SciVis Contest 2023 - Dataset of Neuronal Network Simulations of the Human Brain". Zenodo, Okt. 19, 2022. doi: 10.5281/zenodo.10519411.](https://zenodo.org/records/10519411)
+1. [<span id="reference_dataset">T. Gerrits, F. Czappa, D. Baneshund F. Wolf, "IEEE SciVis Contest 2023 - Dataset of Neuronal Network Simulations of the Human Brain". Zenodo, Okt. 19, 2022. doi: 10.5281/zenodo.10519411.](https://zenodo.org/records/10519411)
+2. [<span id="plasticity">Markus Butz, Arjen van Ooyen, 2013, "A simple rule for dendritic spine and axonal bouton formation can account for cortical reorganization after focal retinal lesions", doi: https://doi.org/10.1371/journal.pcbi.1003259</span>](https://doi.org/10.1371/journal.pcbi.1003259)
--- database/tensor_glyphs/resources/tensor_glyphs_trace.py
+++ database/tensor_glyphs/resources/tensor_glyphs_trace.py
@@ -1,101 +1,198 @@
-# trace generated using paraview version 5.10.1
-#import paraview
-#paraview.compatibility.major = 5
-#paraview.compatibility.minor = 10
-import sys
-from os.path import exists
+# state file generated using paraview version 5.12.0
+import paraview
+paraview.compatibility.major = 5
+paraview.compatibility.minor = 12
 
 #### import the simple module from the paraview
-import paraview.simple as pvs
-
-if len(sys.argv) < 2:
-    print('no data file path provided - defaulting to "./data/jet.vti"')
-    filepath = './data/jet.vti'
-else:
-    filepath = str(sys.argv[1])
-
-file_exists = exists(filepath)
-if not file_exists:
-    print("file ", filepath," does not exist", file=sys.stderr)
-
-# ----------------------------------------------------------------
-# setup the data processing pipelines
-# ----------------------------------------------------------------
-
-# create a new 'XML Image Data Reader'
-reader = pvs.XMLImageDataReader(registrationName='reader', FileName=[filepath])
-reader.PointArrayStatus = ['ImageFile']                                             # OWN_DATA: every field has a name in a .vti file. Replace 'Scalars_' by the corresponding name
-reader.TimeArray = 'None'
-
-# create a new 'Glyph'
-glyph1 = pvs.Glyph(registrationName='Glyph1', Input=reader, GlyphType='Arrow')
-glyph1.OrientationArray = ['POINTS', 'ImageFile']
-glyph1.ScaleArray = ['POINTS', 'ImageFile']
-glyph1.ScaleFactor = 0.024047037060938886
-glyph1.GlyphTransform = 'Transform2'
-
-pvs.UpdatePipeline(time=0.0, proxy=glyph1)
-
-# ----------------------------------------------------------------
-# setup the views
-# ----------------------------------------------------------------
-
+from paraview.simple import *
 #### disable automatic camera reset on 'Show'
-pvs._DisableFirstRenderCameraReset()
-pvs.LoadPalette('WhiteBackground')
+paraview.simple._DisableFirstRenderCameraReset()
 
-renderView1 = pvs.GetActiveViewOrCreate('RenderView')
-renderView1.CameraPosition = [4.5, 1.0381428503314964, 0.5006610233103856]
-renderView1.CameraFocalPoint = [0.4906297167763114, 1.0381428503314964, 0.5006610233103856]
-renderView1.CameraViewUp = [0.0, 0.0, 1.0]
-renderView1.CameraViewAngle = 20.36067481093659
-renderView1.CameraParallelScale = 1.263494633251814
+# ----------------------------------------------------------------
+# setup views used in the visualization
+# ----------------------------------------------------------------
+
+# get the material library
+materialLibrary1 = GetMaterialLibrary()
+
+# Create a new 'Render View'
+renderView1 = CreateView('RenderView')
+renderView1.ViewSize = [1149, 785]
+renderView1.AxesGrid = 'Grid Axes 3D Actor'
+renderView1.CenterOfRotation = [16.720206636353396, 16.720206578145735, 16.72025606688112]
+renderView1.StereoType = 'Crystal Eyes'
+renderView1.CameraPosition = [-62.273695794486905, 84.32590062949528, -70.08976914496148]
+renderView1.CameraFocalPoint = [14.451357128357623, 15.786878937657141, 17.97062784513119]
+renderView1.CameraViewUp = [0.31031897035270395, 0.8620974178744707, 0.4006122548467634]
+renderView1.CameraFocalDisk = 1.0
+renderView1.CameraParallelScale = 28.96664975823481
+renderView1.LegendGrid = 'Legend Grid Actor'
+renderView1.EnableRayTracing = 1
+renderView1.BackEnd = 'OptiX pathtracer'
+renderView1.SamplesPerPixel = 2
+renderView1.OSPRayMaterialLibrary = materialLibrary1
+
+SetActiveView(None)
 
 # ----------------------------------------------------------------
 # setup view layouts
 # ----------------------------------------------------------------
 
 # create new layout object 'Layout #1'
-layout1 = pvs.CreateLayout(name='Layout #1')
+layout1 = CreateLayout(name='Layout #1')
 layout1.AssignView(0, renderView1)
-layout1.SetSize(1920, 1080)
+layout1.SetSize(1149, 785)
+
+# ----------------------------------------------------------------
+# restore active view
+SetActiveView(renderView1)
+# ----------------------------------------------------------------
+
+# ----------------------------------------------------------------
+# setup the data processing pipelines
+# ----------------------------------------------------------------
+
+# create a new 'Legacy VTK Reader'
+atomsvtk = LegacyVTKReader(registrationName='atoms.vtk', FileNames=['D:/Projects/LAMMPS/atoms.vtk'])
+
+# create a new 'Tensor Glyph'
+tensorGlyph1 = TensorGlyph(registrationName='TensorGlyph1', Input=atomsvtk,
+    GlyphType='Superquadric')
+tensorGlyph1.Tensors = ['POINTS', 'STRESS']
+tensorGlyph1.Scalars = ['POINTS', '1']
+tensorGlyph1.Colorby = 'eigenvalues'
+tensorGlyph1.ScaleFactor = 0.001255177081143987
+tensorGlyph1.LimitScalingByEigenvalues = 1
+tensorGlyph1.MaxScaleFactor = 2.0
+
+# init the 'Superquadric' selected for 'GlyphType'
+tensorGlyph1.GlyphType.Thickness = 0.480052
+tensorGlyph1.GlyphType.Toroidal = 0
 
 # ----------------------------------------------------------------
 # setup the visualization in view 'renderView1'
 # ----------------------------------------------------------------
 
-# get color transfer function/color map for 'ImageFile'
-imageFileLUT = pvs.GetColorTransferFunction('ImageFile')                # OWN_DATA: change field name
+# show data from tensorGlyph1
+tensorGlyph1Display = Show(tensorGlyph1, renderView1, 'GeometryRepresentation')
 
-# show data from reader
-readerDisplay = pvs.Show(reader, renderView1, 'UniformGridRepresentation')
-readerDisplay.Representation = 'Outline'
+# get 2D transfer function for 'MaxEigenvalue'
+maxEigenvalueTF2D = GetTransferFunction2D('MaxEigenvalue')
 
-# show data from glyph1
-glyph1Display = pvs.Show(glyph1, renderView1, 'GeometryRepresentation')
-glyph1Display.Representation = 'Surface'
-glyph1Display.ColorArrayName = ['POINTS', 'ImageFile']                  # OWN_DATA: change field
-glyph1Display.LookupTable = imageFileLUT
-glyph1Display.SelectOrientationVectors = 'ImageFile'                    # OWN_DATA: change field for orientation
-glyph1Display.ScaleFactor = 0.2231481334194541
-glyph1Display.SelectScaleArray = 'ImageFile'                            # OWN_DATA: change field for scaling
-glyph1Display.GlyphType = 'Arrow'
-glyph1Display.GlyphTableIndexArray = 'ImageFile'                        # OWN_DATA: change field
+# get color transfer function/color map for 'MaxEigenvalue'
+maxEigenvalueLUT = GetColorTransferFunction('MaxEigenvalue')
+maxEigenvalueLUT.TransferFunction2D = maxEigenvalueTF2D
+maxEigenvalueLUT.RGBPoints = [-1.973176121711731, 0.231373, 0.298039, 0.752941, 0.013411939144134521, 0.865003, 0.865003, 0.865003, 2.0, 0.705882, 0.0156863, 0.14902]
+maxEigenvalueLUT.ScalarRangeInitialized = 1.0
 
-# get color legend/bar for imageFileLUT in view renderView1
-imageFileLUTColorBar = pvs.GetScalarBar(imageFileLUT, renderView1)
-imageFileLUTColorBar.Title = 'Velocity'                                 # OWN_DATA: change the name of the color bar accoridng to what value is visualized
-imageFileLUTColorBar.WindowLocation = 'Any Location'
-imageFileLUTColorBar.Position = [0.94, 0.1]
-imageFileLUTColorBar.ScalarBarLength = 0.8
-imageFileLUTColorBar.ScalarBarThickness = 10
-imageFileLUTColorBar.TitleFontSize = 18
-imageFileLUTColorBar.LabelFontSize = 12
+# trace defaults for the display properties.
+tensorGlyph1Display.Representation = 'Surface'
+tensorGlyph1Display.ColorArrayName = ['POINTS', 'MaxEigenvalue']
+tensorGlyph1Display.LookupTable = maxEigenvalueLUT
+tensorGlyph1Display.Interpolation = 'PBR'
+tensorGlyph1Display.SelectTCoordArray = 'None'
+tensorGlyph1Display.SelectNormalArray = 'Normals'
+tensorGlyph1Display.SelectTangentArray = 'None'
+tensorGlyph1Display.OSPRayScaleArray = 'Normals'
+tensorGlyph1Display.OSPRayScaleFunction = 'Piecewise Function'
+tensorGlyph1Display.Assembly = ''
+tensorGlyph1Display.SelectOrientationVectors = 'None'
+tensorGlyph1Display.ScaleFactor = 3.3455792147666217
+tensorGlyph1Display.SelectScaleArray = 'None'
+tensorGlyph1Display.GlyphType = 'Arrow'
+tensorGlyph1Display.GlyphTableIndexArray = 'None'
+tensorGlyph1Display.GaussianRadius = 0.1672789607383311
+tensorGlyph1Display.SetScaleArray = ['POINTS', 'Normals']
+tensorGlyph1Display.ScaleTransferFunction = 'Piecewise Function'
+tensorGlyph1Display.OpacityArray = ['POINTS', 'Normals']
+tensorGlyph1Display.OpacityTransferFunction = 'Piecewise Function'
+tensorGlyph1Display.DataAxesGrid = 'Grid Axes Representation'
+tensorGlyph1Display.PolarAxes = 'Polar Axes Representation'
+tensorGlyph1Display.SelectInputVectors = ['POINTS', 'Normals']
+tensorGlyph1Display.WriteLog = ''
+tensorGlyph1Display.InputVectors = ['POINTS', 'Normals']
 
-pvs.ResetCamera(renderView1)                                            # OWN_DATA: if the original view does not fit ResetCamera can be used to focus on the visible data
+# init the 'Piecewise Function' selected for 'ScaleTransferFunction'
+tensorGlyph1Display.ScaleTransferFunction.Points = [-1.0, 0.0, 0.5, 0.0, 1.0, 1.0, 0.5, 0.0]
 
-# save screenshot
-pvs.SaveScreenshot('./output/glyphs.png', 
-    layout1, 
-    ImageResolution=[1920, 1080],
-    FontScaling='Do not scale fonts')
+# init the 'Piecewise Function' selected for 'OpacityTransferFunction'
+tensorGlyph1Display.OpacityTransferFunction.Points = [-1.0, 0.0, 0.5, 0.0, 1.0, 1.0, 0.5, 0.0]
+
+# setup the color legend parameters for each legend in this view
+
+# get color legend/bar for maxEigenvalueLUT in view renderView1
+maxEigenvalueLUTColorBar = GetScalarBar(maxEigenvalueLUT, renderView1)
+maxEigenvalueLUTColorBar.Title = 'MaxEigenvalue'
+maxEigenvalueLUTColorBar.ComponentTitle = ''
+
+# set color bar visibility
+maxEigenvalueLUTColorBar.Visibility = 1
+
+# show color legend
+tensorGlyph1Display.SetScalarBarVisibility(renderView1, True)
+
+# ----------------------------------------------------------------
+# setup color maps and opacity maps used in the visualization
+# note: the Get..() functions create a new object, if needed
+# ----------------------------------------------------------------
+
+# get opacity transfer function/opacity map for 'MaxEigenvalue'
+maxEigenvaluePWF = GetOpacityTransferFunction('MaxEigenvalue')
+maxEigenvaluePWF.Points = [-1.973176121711731, 0.0, 0.5, 0.0, 2.0, 1.0, 0.5, 0.0]
+maxEigenvaluePWF.ScalarRangeInitialized = 1
+
+# ----------------------------------------------------------------
+# setup animation scene, tracks and keyframes
+# note: the Get..() functions create a new object, if needed
+# ----------------------------------------------------------------
+
+# get time animation track
+timeAnimationCue1 = GetTimeTrack()
+
+# initialize the animation scene
+
+# get the time-keeper
+timeKeeper1 = GetTimeKeeper()
+
+# initialize the timekeeper
+
+# initialize the animation track
+
+# get animation scene
+animationScene1 = GetAnimationScene()
+
+# initialize the animation scene
+animationScene1.ViewModules = renderView1
+animationScene1.Cues = timeAnimationCue1
+animationScene1.AnimationTime = 0.0
+
+# ----------------------------------------------------------------
+# restore active source
+SetActiveSource(tensorGlyph1)
+# ----------------------------------------------------------------
+
+
+##--------------------------------------------
+## You may need to add some code at the end of this python script depending on your usage, eg:
+#
+## Render all views to see them appears
+# RenderAllViews()
+#
+## Interact with the view, usefull when running from pvpython
+# Interact()
+#
+## Save a screenshot of the active view
+# SaveScreenshot("path/to/screenshot.png")
+#
+## Save a screenshot of a layout (multiple splitted view)
+# SaveScreenshot("path/to/screenshot.png", GetLayout())
+#
+## Save all "Extractors" from the pipeline browser
+# SaveExtracts()
+#
+## Save a animation of the current active view
+# SaveAnimation()
+#
+## Please refer to the documentation of paraview.simple
+## https://kitware.github.io/paraview-docs/latest/python/paraview.simple.html
+##--------------------------------------------
