c33155c4c650e8e071008edd97db3bc211f2079f
Jens Koenen <je.koenen2@gmail.com>
2024-04-28 23:26:58 +0200

    Worked on error page

--- website/pages/error.vue
+++ website/pages/error.vue
@@ -1,3 +1,30 @@
+<script>
+    import GlobalHeader from "../components/global_header.vue"
+    import GlobalFooter from "../components/global_footer.vue"
+
+    export default
+    {
+        components:
+        {
+            GlobalHeader,
+            GlobalFooter
+        }
+    };
+</script>
 <template>
-    Error
+    <header>
+        <GlobalHeader></GlobalHeader>
+    </header>
+    <main class="flex-fill">
+        <div class="container ps-lg-5 py-5">
+            <h1 class="display-1 py-5 mb-4 fw-semibold">Error</h1>
+            <p class="col-lg-8" style="text-align: justify;">
+                The page that you want to access ether does not exist or an error occured during the usage of the website.
+            </p>
+            <a href="/">Return to Homepage</a>
+        </div>
+    </main>
+    <footer class="bg-body-tertiary mt-4">
+        <GlobalFooter class="container"></GlobalFooter>
+    </footer>
 </template>
