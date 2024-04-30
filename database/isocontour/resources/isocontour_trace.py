4a296b60f93987ef6ee5c6fee537903fb1a71135
Marvin Petersen <m_petersen12@cs.uni-kl.de>
2024-04-30 15:04:06 +0200

    Add color bar to isocontour example

--- database/isocontour/resources/isocontour_trace.py
+++ database/isocontour/resources/isocontour_trace.py
@@ -58,21 +58,39 @@ renderView1.CameraParallelScale = 220.83647796503186
 # create new layout object 'Layout #1'
 layout1 = pvs.CreateLayout(name='Layout #1')
 layout1.AssignView(0, renderView1)
-layout1.SetSize(825, 1176)
+layout1.SetSize(1920, 1080)
 
 # ----------------------------------------------------------------
 # setup the visualization in view 'renderView1'
 # ----------------------------------------------------------------
 
+# get 2D transfer function for 'Scalars_'
+scalars_TF2D = pvs.GetTransferFunction2D('Scalars_')                    # OWN_DATA: change field
+
+# get color transfer function/color map for 'Scalars_'
+scalars_LUT = pvs.GetColorTransferFunction('Scalars_')                  # OWN_DATA: change field
+scalars_LUT.TransferFunction2D = scalars_TF2D
+scalars_LUT.RGBPoints = [0.0, 0.231373, 0.298039, 0.752941, 127.5, 0.865003, 0.865003, 0.865003, 255.0, 0.705882, 0.0156863, 0.14902]
+scalars_LUT.ScalarRangeInitialized = 1.0
+
 # show data from contour1
 contour1Display = pvs.Show(contour1, renderView1, 'GeometryRepresentation')
 contour1Display.Representation = 'Surface'
-contour1Display.ColorArrayName = ['POINTS', 'Scalars_']             # OWN_DATA: change field
+contour1Display.ColorArrayName = ['POINTS', 'Scalars_']                 # OWN_DATA: change field
+contour1Display.SetScalarBarVisibility(renderView1, True)
 
-# pvs.ResetCamera(renderView1)                                      # OWN_DATA: if the original view does not fit ResetCamera can be used to focus on the visible data
+# get color legend/bar for scalars_LUT in view renderView1
+scalars_LUTColorBar = pvs.GetScalarBar(scalars_LUT, renderView1)
+scalars_LUTColorBar.WindowLocation = 'Any Location'
+scalars_LUTColorBar.Position = [0.8755364806866953, 0.022857142857142857]
+scalars_LUTColorBar.Title = 'density'                                   # OWN_DATA: change title
+scalars_LUTColorBar.ComponentTitle = ''
+scalars_LUTColorBar.ScalarBarLength = 0.9147619047619047
+scalars_LUTColorBar.Visibility = 1
+
+# pvs.ResetCamera(renderView1)                                          # OWN_DATA: if the original view does not fit ResetCamera can be used to focus on the visible data
 
 # save screenshot
 pvs.SaveScreenshot('./output/isocontour.png', 
-    layout1, 
-    ImageResolution=[1920, 1080],
+    layout1,
     FontScaling='Do not scale fonts')
